d63c9679ffb9aea35e77980953e9abdf2077fea8
Edison Cai <edisoncai2001@gmail.com>
2021-06-21 20:29:54 +0100

    importing hand tracking module

--- volume_hand_control.py
+++ volume_hand_control.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import numpy as np
+import hand_tracking_module as htm
 
 ########################
 camera_width, camera_height = 640, 480
@@ -20,6 +21,9 @@ while True:
     current_time = time.time()
     fps = 1 / (current_time - previous_time)
     previous_time = current_time
+    # Drawing the FPS
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                1, (255, 0, 0), 3)
 
     cv2.imshow("Image", cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE))
     cv2.waitKey(1)
