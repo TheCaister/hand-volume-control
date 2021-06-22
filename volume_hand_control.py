74f62cfc1cebbe713b4d7f09b2408d20b6886060
Edison Cai <edisoncai2001@gmail.com>
2021-06-22 21:32:34 +0100

    import pycaw to manipulate volume and other graphics
    Also calculating length between index and thumb which can subsequently be interpolated to the volume levels, volume bar length and volume percentage. These values can then be passed into some cv2 functions to graphically represent the volume to the user

--- volume_hand_control.py
+++ volume_hand_control.py
@@ -2,6 +2,24 @@ import cv2
 import time
 import numpy as np
 import hand_tracking_module as htm
+import math
+
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+volume_range = volume.GetVolumeRange()
+minimum_volume = volume_range[0]
+maximum_volume = volume_range[1]
+vol = 0
+volume_bar = 400
+volume_percentage = 0
 
 ########################
 camera_width, camera_height = 640, 480
@@ -33,8 +51,37 @@ while True:
         x1, y1 = landmarks_list[4][1], landmarks_list[4][2]
         x2, y2 = landmarks_list[8][1], landmarks_list[8][2]
 
+        # Getting the centre point of the two positions
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        # Drawing circles on these positions
         cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
         cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+        # Drawing a line and a circle between the two tips
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        # Getting the length between the two points
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        # Convert length in the hand range to volume range using interpolation
+        vol = np.interp(length, [50, 300], [minimum_volume, maximum_volume])
+        # Also converting to range of height of the volume bar
+        volume_bar = np.interp(length, [50, 300], [400, 150])
+        # And converting to percentage
+        volume_percentage = np.interp(length, [50, 300], [0, 100])
+
+        volume.SetMasterVolumeLevel(vol, None)
+
+        if length < 50:
+            cv2.circle(img, (cy, cy), 15, (0, 255, 0), cv2.FILLED)
+
+    # Drawing a rectangle representing the volume
+    cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
+    cv2.rectangle(img, (50, int(volume_bar)), (85, 400), (0, 255, 0), cv2.FILLED)
+    # Drawing percentage
+    cv2.putText(img, f'{int(volume_percentage)}', (40, 450), cv2.FONT_HERSHEY_SIMPLEX,
+                1, (0, 255, 0), 2)
 
 
 # Getting the FPS
